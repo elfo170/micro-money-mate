9d2965b11b1f2b7aeec5a69e5b8a0bd705a385fb
elfo170 <leormacieira@hotmail.com>
2025-07-16 15:33:33 -0300

    Fix compile 3

--- android/app/src/main/java/com/micromoneymate/app/capturaNotificacao/capturaNotificacao_Plugin.java
+++ android/app/src/main/java/com/micromoneymate/app/capturaNotificacao/capturaNotificacao_Plugin.java
@@ -2,6 +2,7 @@ package com.micromoneymate.app.capturaNotificacao;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
